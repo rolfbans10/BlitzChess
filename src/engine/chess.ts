00b83ec900404cef5044ec55d46023a2ed1c3c08
Rolf Bansbach <rolfbans10@gmail.com>
2025-03-27 23:38:08 -0400

    setupInitialPosition

--- src/engine/chess.ts
+++ src/engine/chess.ts
@@ -77,6 +77,163 @@ export const setupInitialPositions = (): Square[][] => {
     };
   }
 
+  // rooks
+  board[0][0].piece = {
+    type: PieceType.ROOK,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 0,
+    },
+  };
+
+  board[0][7].piece = {
+    type: PieceType.ROOK,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 7,
+    },
+  };
+
+  board[7][0].piece = {
+    type: PieceType.ROOK,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 0,
+    },
+  };
+
+  board[7][7].piece = {
+    type: PieceType.ROOK,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 7,
+    },
+  };
+
+  // knights
+  board[0][1].piece = {
+    type: PieceType.KNIGHT,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 1,
+    },
+  };
+  board[0][6].piece = {
+    type: PieceType.KNIGHT,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 6,
+    },
+  };
+  board[7][1].piece = {
+    type: PieceType.KNIGHT,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 1,
+    },
+  };
+  board[7][6].piece = {
+    type: PieceType.KNIGHT,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 6,
+    },
+  };
+
+  // bishops
+  board[0][2].piece = {
+    type: PieceType.BISHOP,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 2,
+    },
+  };
+  board[0][5].piece = {
+    type: PieceType.BISHOP,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 5,
+    },
+  };
+  board[7][2].piece = {
+    type: PieceType.BISHOP,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 2,
+    },
+  };
+  board[7][5].piece = {
+    type: PieceType.BISHOP,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 5,
+    },
+  };
+
+  // queens
+  board[0][3].piece = {
+    type: PieceType.QUEEN,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 3,
+    },
+  };
+  board[7][3].piece = {
+    type: PieceType.QUEEN,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 3,
+    },
+  };
+
+  // kings
+  board[0][4].piece = {
+    type: PieceType.KING,
+    color: PieceColor.WHITE,
+    captured: false,
+    pos: {
+      x: 0,
+      y: 4,
+    },
+  };
+  board[7][4].piece = {
+    type: PieceType.KING,
+    color: PieceColor.BLACK,
+    captured: false,
+    pos: {
+      x: 7,
+      y: 4,
+    },
+  };
+
   return board;
 };
 
